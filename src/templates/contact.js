9da447914c2041e9672826850bf3e9580849353e
saisat <saivedam@outlook.com>
2019-08-15 22:03:45 +0530

    Hid contact form. removed copyright.

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -14,20 +14,20 @@ export default class Contact extends React.Component {
                 <article class="post page post-full">
                   <header class="post-header inner-small">
                     <h1 class="post-title line-top">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
-                    {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
+                    {_.get(this.props, 'pageContext.frontmatter.subtitle') &&
                     <div class="post-subtitle">
                       {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle'))}
                     </div>
                     }
                   </header>
-                  {_.get(this.props, 'pageContext.frontmatter.img_path') && 
+                  {_.get(this.props, 'pageContext.frontmatter.img_path') &&
                   <div class="post-thumbnail">
                     <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.img_path'))} alt={_.get(this.props, 'pageContext.frontmatter.title')} />
                   </div>
                   }
                   <div class="post-content inner-small">
                     {htmlToReact(_.get(this.props, 'pageContext.html'))}
-                    <ContactForm {...this.props} />
+                    {/*<ContactForm {...this.props} />*/}
                   </div>
                 </article>
               </div>
